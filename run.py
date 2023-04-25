1944a0d858f282f27b153da4a575e408edbed59f
Nam Cao <namcaov@gmail.com>
2023-04-25 15:39:13 +0200

    print currently simulated file

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ elf_files = os.listdir("files")
 
 for f in elf_files:
     report_name = f.split('.')[0] + ".rpt"
+    print("Simulating ", f)
     os.system("./build/armory_example --armv7m files/{} > reports/{}".format(f, report_name))
