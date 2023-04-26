bd27f99e8abaeaacb57f5938935e258572bece1f
Nam Cao <namcaov@gmail.com>
2023-04-26 12:28:33 +0200

    update run.py with new files/ structure

--- run.py
+++ run.py
@@ -2,10 +2,12 @@ import os
 from pathlib import Path
 
 Path("reports").mkdir(exist_ok=True)
+Path("reports/gcc_o2").mkdir(exist_ok=True)
+Path("reports/gcc_o3").mkdir(exist_ok=True)
+Path("reports/gcc_os").mkdir(exist_ok=True)
 
-elf_files = os.listdir("files")
-
-for f in elf_files:
-    report_name = f.split('.')[0] + ".rpt"
-    print("Simulating ", f)
-    os.system("./build/armory_example --armv7m files/{} > reports/{}".format(f, report_name))
+for dir in os.listdir("files"):
+    for f in os.listdir(os.path.join("files", dir)):
+        report_name = os.path.join("reports", dir, f.split('.')[0] + ".rpt")
+        print("Simulating ", f)
+        os.system("./build/armory_example --armv7m {} > {}".format(os.path.join("files", dir, f), report_name))
