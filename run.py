febad59f2b8581ec5907498e6f1c9e1321789033
Nam Cao <namcaov@gmail.com>
2023-04-28 20:44:14 +0200

    add o1 and o2 to run.py

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ import os
 from pathlib import Path
 
 Path("reports").mkdir(exist_ok=True)
+Path("reports/gcc_o0").mkdir(exist_ok=True)
+Path("reports/gcc_o1").mkdir(exist_ok=True)
 Path("reports/gcc_o2").mkdir(exist_ok=True)
 Path("reports/gcc_o3").mkdir(exist_ok=True)
 Path("reports/gcc_os").mkdir(exist_ok=True)
+Path("reports/clang_o0").mkdir(exist_ok=True)
+Path("reports/clang_o1").mkdir(exist_ok=True)
 Path("reports/clang_o2").mkdir(exist_ok=True)
 Path("reports/clang_o3").mkdir(exist_ok=True)
 Path("reports/clang_os").mkdir(exist_ok=True)
