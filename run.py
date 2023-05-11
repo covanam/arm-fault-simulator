082e419157336e3ce939ddf2de010cc247877181
Nam Cao <namcaov@gmail.com>
2023-05-11 18:13:29 +0200

    switch to run swift files

--- run.py
+++ run.py
@@ -13,8 +13,8 @@ Path("reports/clang_o2").mkdir(exist_ok=True)
 Path("reports/clang_o3").mkdir(exist_ok=True)
 Path("reports/clang_os").mkdir(exist_ok=True)
 
-for dir in os.listdir("rasm_files"):
-    for f in os.listdir(os.path.join("rasm_files", dir)):
+for dir in os.listdir("swift_files"):
+    for f in os.listdir(os.path.join("swift_files", dir)):
         report_name = os.path.join("reports", dir, f.split('.')[0] + ".rpt")
         print("Simulating ", f)
-        os.system("./build/armory_example --armv7m {} > {}".format(os.path.join("rasm_files", dir, f), report_name))
+        os.system("./build/armory_example --armv7m {} > {}".format(os.path.join("swift_files", dir, f), report_name))
